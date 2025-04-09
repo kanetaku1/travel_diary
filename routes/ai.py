20d09ba5c73842ff1021a802c42918cf0dafd4fc
kanetaku1 <ublvsp010111@gmail.com>
2025-04-10 00:13:43 +0900

    Cloud Vision APIによりタグの自動生成が完成

--- routes/ai.py
+++ routes/ai.py
@@ -1,43 +1,32 @@
-import os
+from google.cloud import vision
 from fastapi import APIRouter, UploadFile, HTTPException
-from typing import List
-from openai import OpenAI
-import base64
 
 router = APIRouter()
 
-API_KEY = os.getenv("OPEN_API_KEY")
-
-client = OpenAI(
-  api_key = API_KEY
-)
-
-@router.post("/generate_tags/")
+@router.post("/generate_tags")
 async def generate_tags(file: UploadFile):
   try:
-    # 画像データをAPIに送信
+    # 画像をバイトデータに変換し、Base64エンコード
+    client = vision.ImageAnnotatorClient()
     image_data = await file.read()
-    encoded_image = base64.b64encode(image_data).decode("utf-8")
-    response = client.chat.completions.create(
-      model="gpt-4o-vision-preview",
-      messages=[
-        {"role": "system", "content": "あなたは画像からタグを生成するAIです。"},
-        {"role": "user", 
-         "content": [
-           {"type": "text", "text": "この画像のタグを生成してください。"},
-           {"type": "image_url", "image_url": f"data:image/jpeg;base64,{encoded_image}"}
-         ]
-        }
-      ],
-    )
-    choices = response.choices
-    if choices:
-      content = choices[0].message.content
-      print(content)
-      tags = content.split(", ")
-      return {"tags": tags}
-    else:
-      raise HTTPException(status_code=500, detail="No choices found in the response")
+    image = vision.Image(content=image_data)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print("labels:")
+
+    for label in labels:
+      print(label.description)
+    
+    if response.error.message:
+      raise Exception(
+        "{}\nFor more info on error messages, check: "
+        "https://cloud.google.com/apis/design/errors".format(response.error.message)
+      )
+    # タグを取得
+    tags = [label.description for label in labels]
+    print("tags:")
+    return {"tags": tags}
+
   except Exception as e:
-    print(f"Error: {str(e)}")  # エラーメッセージをログに出力
     raise HTTPException(status_code=500, detail=str(e))
